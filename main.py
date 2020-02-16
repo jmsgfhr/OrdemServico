d67c74fc2946e79273f3b454c906ad6898fc7909
Jonathan Meirelles <jonathan@pop-os.localdomain>
2020-02-16 14:28:40 -0300

    editando o pdf

--- main.py
+++ main.py
@@ -1,18 +1,50 @@
 from tkinter import  *
 from datetime import datetime
-from fpdf import FPDF
 import json
 from functools import partial
+from fpdf import Template
 
 Today = datetime.now()
 
 Window = Tk()
 
-pdf = FPDF( orientation='P', unit='mm', format='A4' )
-pdf.add_page()
-pdf.set_font("Arial", size=12)
-pdf.cell(200, 10, txt="oi", ln=1, align="C")
-pdf.output("simple_demo.pdf")
+#this will define the ELEMENTS that will compose the template. 
+elements = [
+    { 'name': 'company_logo', 'type': 'I', 'x1': 20.0, 'y1': 17.0, 'x2': 78.0, 'y2': 30.0, 'font': None, 'size': 0.0, 'bold': 0, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': 'logo', 'priority': 2, },
+    { 'name': 'company_name', 'type': 'T', 'x1': 20.0, 'y1': 32.5, 'x2': 115.0, 'y2': 37.5, 'font': 'Arial', 'size': 12.0, 'bold': 1, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': '', 'priority': 2, },
+    { 'name': 'client_name', 'type': 'T', 'x1': 20.0, 'y1': 40, 'x2': 115.0, 'y2': 37.5, 'font': 'Arial', 'size': 12.0, 'bold': 1, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': '', 'priority': 2, },
+    { 'name': 'client_certified', 'type': 'T', 'x1': 20.0, 'y1': 40, 'x2': 115.0, 'y2': 37.5, 'font': 'Arial', 'size': 12.0, 'bold': 1, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': '', 'priority': 2, },
+    { 'name': 'client_phone', 'type': 'T', 'x1': 20.0, 'y1': 50, 'x2': 115.0, 'y2': 37.5, 'font': 'Arial', 'size': 12.0, 'bold': 1, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': '', 'priority': 2, },
+    { 'name': 'client_email', 'type': 'T', 'x1': 20.0, 'y1': 50, 'x2': 115.0, 'y2': 37.5, 'font': 'Arial', 'size': 12.0, 'bold': 1, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': '', 'priority': 2, },
+    { 'name': 'client_adress', 'type': 'T', 'x1': 20.0, 'y1': 60, 'x2': 115.0, 'y2': 37.5, 'font': 'Arial', 'size': 12.0, 'bold': 1, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': '', 'priority': 2, },
+    { 'name': 'client_cep', 'type': 'T', 'x1': 20.0, 'y1': 60, 'x2': 115.0, 'y2': 37.5, 'font': 'Arial', 'size': 12.0, 'bold': 1, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': '', 'priority': 2, },
+    { 'name': 'date', 'type': 'T', 'x1': 20.0, 'y1': 70, 'x2': 115.0, 'y2': 37.5, 'font': 'Arial', 'size': 12.0, 'bold': 1, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': '', 'priority': 2, },
+    { 'name': 'client_city', 'type': 'T', 'x1': 20.0, 'y1': 70, 'x2': 115.0, 'y2': 37.5, 'font': 'Arial', 'size': 12.0, 'bold': 1, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': '', 'priority': 2, },
+    { 'name': 'client_district', 'type': 'T', 'x1': 20.0, 'y1': 70, 'x2': 115.0, 'y2': 37.5, 'font': 'Arial', 'size': 12.0, 'bold': 1, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': '', 'priority': 2, },
+    { 'name': 'service_type', 'type': 'T', 'x1': 20.0, 'y1': 80, 'x2': 115.0, 'y2': 37.5, 'font': 'Arial', 'size': 12.0, 'bold': 1, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': '', 'priority': 2, },
+    { 'name': 'service_local', 'type': 'T', 'x1': 20.0, 'y1': 80, 'x2': 115.0, 'y2': 37.5, 'font': 'Arial', 'size': 12.0, 'bold': 1, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': '', 'priority': 2, },
+    { 'name': 'service_priority', 'type': 'T', 'x1': 20.0, 'y1': 90, 'x2': 115.0, 'y2': 37.5, 'font': 'Arial', 'size': 12.0, 'bold': 1, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': '', 'priority': 2, },
+    { 'name': 'service_model', 'type': 'T', 'x1': 20.0, 'y1': 90, 'x2': 115.0, 'y2': 37.5, 'font': 'Arial', 'size': 12.0, 'bold': 1, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': '', 'priority': 2, },
+    { 'name': 'service_call', 'type': 'T', 'x1': 20.0, 'y1': 90, 'x2': 115.0, 'y2': 37.5, 'font': 'Arial', 'size': 12.0, 'bold': 1, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': '', 'priority': 2, },
+    { 'name': 'serial_number', 'type': 'T', 'x1': 20.0, 'y1': 100, 'x2': 115.0, 'y2': 37.5, 'font': 'Arial', 'size': 12.0, 'bold': 1, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': '', 'priority': 2, },
+    { 'name': 'acessories', 'type': 'T', 'x1': 20.0, 'y1': 110, 'x2': 115.0, 'y2': 37.5, 'font': 'Arial', 'size': 12.0, 'bold': 1, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': '', 'priority': 2, },
+    { 'name': 'related_problem', 'type': 'T', 'x1': 20.0, 'y1': 120, 'x2': 115.0, 'y2': 37.5, 'font': 'Arial', 'size': 12.0, 'bold': 1, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': '', 'priority': 2, },
+    { 'name': 'problem', 'type': 'T', 'x1': 20.0, 'y1': 120, 'x2': 115.0, 'y2': 37.5, 'font': 'Arial', 'size': 12.0, 'bold': 1, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': '', 'priority': 2, },
+    { 'name': 'service_parts', 'type': 'T', 'x1': 20.0, 'y1': 130, 'x2': 115.0, 'y2': 37.5, 'font': 'Arial', 'size': 12.0, 'bold': 1, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': '', 'priority': 2, },
+    { 'name': 'observation', 'type': 'T', 'x1': 20.0, 'y1': 140, 'x2': 115.0, 'y2': 37.5, 'font': 'Arial', 'size': 12.0, 'bold': 1, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': '', 'priority': 2, },
+    { 'name': 'payment_type', 'type': 'T', 'x1': 20.0, 'y1': 150, 'x2': 115.0, 'y2': 37.5, 'font': 'Arial', 'size': 12.0, 'bold': 1, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': '', 'priority': 2, },
+    { 'name': 'status', 'type': 'T', 'x1': 20.0, 'y1': 150, 'x2': 115.0, 'y2': 37.5, 'font': 'Arial', 'size': 12.0, 'bold': 1, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': '', 'priority': 2, },
+    { 'name': 'service_value', 'type': 'T', 'x1': 20.0, 'y1': 160, 'x2': 115.0, 'y2': 37.5, 'font': 'Arial', 'size': 12.0, 'bold': 1, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': '', 'priority': 2, },
+    { 'name': 'service_products', 'type': 'T', 'x1': 20.0, 'y1': 160, 'x2': 115.0, 'y2': 37.5, 'font': 'Arial', 'size': 12.0, 'bold': 1, 'italic': 0, 'underline': 0, 'foreground': 0, 'background': 0, 'align': 'I', 'text': '', 'priority': 2, },
+]
+
+#here we instantiate the template and define the HEADER
+f = Template(format="A4", elements=elements,
+             title="Ordem de Servico")
+f.add_page()
+
+
+
 # Corpo do programa
 
 # Funcoes
@@ -52,6 +84,41 @@ def saveOrdem():
     with open('database.json','w') as BD:
         json.dump(database,BD)
     BD.close()
+    
+    #creating pdf
+    
+    #we FILL some of the fields of the template with the information we want
+    #note we access the elements treating the template instance as a "dict"
+    f["company_name"] = "Inova Service"
+    f["company_logo"] = "img/logo.png"
+    f["client_name"] = EntryName.get()
+    f["client_certified"] = EntryCertified.get()
+    f["client_phone"] = EntryPhone.get()
+    f["client_email"] = EntryEmail.get()
+    f["client_adress"] = EntryAdress.get()
+    f["client_cep"] = EntryCEP.get()
+    f["date"] = LabelDateShow['text']
+    f["client_city"] = EntryCity.get()
+    f["client_district"] = EntryDistrict.get()
+    f["service_type"] = ServicePick.get()
+    f["service_local"] = LocalPick.get()
+    f["service_priority"] = PriorityPick.get()
+    f["service_model"] = EntryModel.get()
+    f["service_call"] = EntryCall.get()
+    f["serial_number"] = EntrySerialNumber.get()
+    f["acessories"] = EntryAcessories.get()
+    f["related_problem"] = EntryRelatedProblem.get(1.0,END)
+    f["problem"] = EntryProblem.get(1.0,END)
+    f["service_parts"] = EntryParts.get(1.0,END)
+    f["observation"] = EntryObservations.get(1.0,END)
+    f["payment_type"] = PaymentPick.get()
+    f["status"] = StatusPick.get()
+    f["service_value"] = EntryServicePrice.get()
+    f["service_products"] = EntryProductPrice.get()
+    fileName = "batata"
+
+    #and now we render the page
+    f.render("./"+fileName+".pdf")
 
 # Area do usuario
 
